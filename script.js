7f9b24d6a61611206e2983990e99b82c242be7c2
Sergei <sergeithejohansen@gmail.com>
2020-10-30 12:22:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log(1);
+
+/* const result = confirm("Are you here?");
+
+console.log(result); */
+
+/* const answer = prompt("how old are you?3131", "18");
+console.log(answer); */
+
+/* const answers = []; */
+
+/* answers[0] = prompt('What is your first name?', '');
+answers[1] = prompt('What is your last name?', '');
+answers[2] = prompt('How old are you?', ''); */
+
+/* document.write(answers); */
+
+console.log(4 + +"44");
